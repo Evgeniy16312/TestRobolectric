ad2031b790f1071ba7f05c035cf85f0884787997
Evgeniy16312 <turin.92@mail.ru>
2022-04-03 21:54:17 +0300

    test liveData

--- app/src/test/java/com/example/testrobolectric/SearchViewModelTest.kt
+++ app/src/test/java/com/example/testrobolectric/SearchViewModelTest.kt
@@ -1,15 +1,18 @@
 package com.example.testrobolectric
 
 import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.testrobolectric.presenter.ScheduleProviderStub
 import com.example.testrobolectric.repository.FakeGitHubRepository
-import com.example.testrobolectric.repository.GitHubRepository
-import com.example.testrobolectric.tests_search.SchedulerProvider
 import com.example.testrobolectric.tests_search.model.SearchResponse
+import com.example.testrobolectric.tests_search.viewmodel.ScreenState
 import com.example.testrobolectric.tests_search.viewmodel.SearchViewModel
 import io.reactivex.Observable
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -22,6 +25,10 @@ import org.robolectric.annotation.Config
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Build.VERSION_CODES.O_MR1])
 class SearchViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var searchViewModel: SearchViewModel
 
     @Mock
@@ -33,7 +40,8 @@ class SearchViewModelTest {
         searchViewModel = SearchViewModel(repository, ScheduleProviderStub())
     }
 
-    @Test //Проверим вызов метода searchGitHub() у нашей ВьюМодели
+    @Test
+//Проверим вызов метода searchGitHub() у нашей ВьюМодели
     fun search_Test() {
         Mockito.`when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(
             Observable.just(
@@ -45,6 +53,52 @@ class SearchViewModelTest {
         )
         searchViewModel.searchGitHub(SEARCH_QUERY)
         verify(repository, times(1)).searchGithub(SEARCH_QUERY)
+    }
+
+    @Test
+    fun liveData_TestReturnValueIsNotNull() {
+//Создаем обсервер. В лямбде мы не вызываем никакие методы - в этом нет необходимости
+//так как мы проверяем работу LiveData и не собираемся ничего делать с данными, которые она возвращает
+        val observer = Observer<ScreenState> {}
+//Получаем LiveData
+        val liveData = searchViewModel.subscribeToLiveData()
+//При вызове Репозитория возвращаем шаблонные данные
+        Mockito.`when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(
+            Observable.just(
+                SearchResponse(
+                    1,
+                    listOf()
+                )
+            )
+        )
+        try {
+//Подписываемся на LiveData без учета жизненного цикла
+            liveData.observeForever(observer)
+            searchViewModel.searchGitHub(SEARCH_QUERY)
+//Убеждаемся, что Репозиторий вернул данные и LiveData передала их Наблюдателям
+            Assert.assertNotNull(liveData.value)
+        } finally {
+//Тест закончен, снимаем Наблюдателя
+            liveData.removeObserver(observer)
+        }
+    }
+
+    @Test
+    fun liveData_TestReturnValueIsError() {
+        val observer = Observer<ScreenState> {}
+        val liveData = searchViewModel.subscribeToLiveData()
+        val error = Throwable(ERROR_TEXT)
+//При вызове Репозитория возвращаем ошибку
+        Mockito.`when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(Observable.error(error))
+        try {
+            liveData.observeForever(observer)
+            searchViewModel.searchGitHub(SEARCH_QUERY)
+//Убеждаемся, что Репозиторий вернул ошибку и LiveData возвращает ошибку
+            val value: ScreenState.Error = liveData.value as ScreenState.Error
+            Assert.assertEquals(value.error.message, error.message)
+        } finally {
+            liveData.removeObserver(observer)
+        }
     }
 
     companion object {
